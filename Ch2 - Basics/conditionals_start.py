8f91f8b6049671e8a9789173e091b4a9b4b93270
Carlie Duperval <ritchellcarlie@gmail.com>
2022-08-05 19:04:38 -0400

    Added one line if else statetment

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -6,21 +6,24 @@
 
 
 def main():
-    x, y = 100, 100
+    x, y = 10, 100
 
     # conditional flow uses if, elif, else
-    if x < y :
-        result = "x is less than y"
-    elif x == y:
-        result = "  x is the same as y"
-    else:
-        result = "x is greater than y"
+    # if x < y :
+    #     result = "x is less than y"
+    # elif x == y:
+    #     result = "  x is the same as y"
+    # else:
+    #     result = "x is greater than y"
 
-    print(result)
+    # print(result)
 
     
 
-    # conditional statements let you use "a if C else b"
+    # conditional statements let you use "a if C else b"//  one line if else statement in python
+    result = "x is less than y" if x < y else "x is greater or equal to y"
+    print(result)
+
 
     # match-case makes it easy to compare multiple values
     value = "one"
