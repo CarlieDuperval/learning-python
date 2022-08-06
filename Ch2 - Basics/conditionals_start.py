7c46b3ec47263824ae8921a502e26b89758eb364
Carlie Duperval <ritchellcarlie@gmail.com>
2022-08-05 20:32:44 -0400

    Added while loop

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -25,8 +25,19 @@ def main():
     print(result)
 
 
+
     # match-case makes it easy to compare multiple values
-    value = "one"
+    value = "42"
+    match value:
+        case "one":
+            result = 1
+        case "two":
+            result = 2
+        case "three" | "four":
+            result = (3, 4)
+        case _ : 
+            result = -1
+    print(result)
 
 if __name__ == "__main__":
     main()
