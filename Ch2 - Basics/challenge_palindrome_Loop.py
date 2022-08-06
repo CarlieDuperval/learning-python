a869d6494d46fafde800754b0b6c7e475e54eaa9
Carlie Duperval <ritchellcarlie@gmail.com>
2022-08-06 09:31:05 -0400

    Added Palindrome solution with for loop

--- Ch2 - Basics/challenge_palindrome_Loop.py
+++ Ch2 - Basics/challenge_palindrome_Loop.py
@@ -2,3 +2,19 @@
 
 # 0- check if the string is Palindrome or not 
 #  run loop from o to length/2: for i in range( 0, int(len(str)/2)) to check if srt[i] != srt[len(srt)-i-1] in this cas return false
+
+from re import S
+
+
+def isPalindrome(string):
+    for i in range(0, int(len(string)/2)):
+        if string[i] != string[len(string)-i-1]:
+            return False
+    return True
+s = "mioim"
+result = isPalindrome(s)
+
+if (result):
+    print("Yes This is a Palindrome")
+else:
+    print("No This is not a Palindrome")
