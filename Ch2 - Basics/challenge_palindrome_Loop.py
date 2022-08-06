cf8a54cd63deb308af69372bf89187fee55ed2a3
Carlie Duperval <ritchellcarlie@gmail.com>
2022-08-06 09:12:05 -0400

    Sudo coding Palindrome with a loop in Python

---
+++ Ch2 - Basics/challenge_palindrome_Loop.py
@@ -0,0 +1,4 @@
+# we will run a loop from starting to length/2 and check the first to the last character of the string
+
+# 0- check if the string is Palindrome or not 
+#  run loop from o to length/2: for i in range( 0, int(len(str)/2)) to check if srt[i] != srt[len(srt)-i-1] in this cas return false
